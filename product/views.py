3c4dd36080db040361ceced04aebd8183592c5bf
Vrag404 <vrag404@protonmail.com>
2022-04-09 13:25:43 -0300

    requirements.txt added

--- product/views.py
+++ product/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-
 from django.contrib import messages
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
